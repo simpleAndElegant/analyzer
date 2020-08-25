595569676d7db02cb1c4bb1414d54d74cbc5ee82
liangrui3 <liangrui3@tal.com>
2020-08-25 19:12:49 +0800

    feat: 初步实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Upload from './components/upload'
+import './App.css'
+
 function App() {
   return (
     <div>
--- src/components/upload/index.tsx
+++ src/components/upload/index.tsx
@@ -1,35 +1,68 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import XLSX from 'xlsx'
-interface targetFile {
+import { Result, Button } from 'antd';
 
+interface targetBlob {
+  busProps: Object;
+  userProps: Object;
+  eventId: String;
+  userName: String;
+  realIp: String;
 }
 
 
 function Upload() {
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState([])
+
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     let fileName:Blob = event.target.files![0]
     let reader = new FileReader()
     reader.readAsBinaryString(fileName)
     reader.onload = function (e: any) {
       let workbook = XLSX.read(e.target.result, {type: 'binary'})
-      let sheetList = workbook.SheetNames
-      const result:any = []
-      console.log(XLSX.utils.sheet_to_json(workbook.Sheets))
-      // workbook.SheetNames.forEach((sheetName) => {
-      //   result.push({
-      //     sheetName: sheetName,
-      //     sheet: XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
-      //   })
-      // })
-      console.log(result)
-
-      // sheetList.forEach(function(y) {
-
-      // })
+      const wsname = workbook.SheetNames[0]; //取第一张表
+      const ws = XLSX.utils.sheet_to_json(workbook.Sheets[wsname]); //生成json表格内容
+      const outputs:[] = ws.map((item: any) => ({
+        usrProps: JSON.parse(item['py_dwd_track_testenv.usr_props']) ,
+        busProps: JSON.parse(item['py_dwd_track_testenv.bus_props']) ,
+        userName: item['py_dwd_track_testenv.user_name'],
+        eventId: item['py_dwd_track_testenv.event_id'],
+        realIp: item['py_dwd_track_testenv.realip'],
+      }))
+      setShowResult(true)
+      setResult(outputs)
     }
   }
+
+
+  const UploadResult =  (
+    <Result
+      status="success"
+      title="Successfully Upload"
+      subTitle="文件上传成功，请选择分析模式"
+      extra={[
+        <Button 
+          type="primary" 
+          onClick={handleFunnelClick}
+          key="funnel"
+        >
+          漏斗分析
+        </Button>,
+        <Button
+          onClick={() => window.location.reload()}
+          key="reset">重新上传</Button>,
+      ]}
+    />
+  )
+
   return (
-    <input onChange={handleChange} type="file" />
+      <>
+        <input onChange={handleChange} type="file" />
+       {showResult ? UploadResult : null}  
+      </>
+ 
   )
 }
 
